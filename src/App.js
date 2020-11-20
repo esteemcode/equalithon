9fba668293f642f24ed6b96a2b91494627cf12ba
Jocelyn Jeriah <jocelynjeriah@jocelyns-mbp.myfiosgateway.com>
2020-11-20 14:46:01 -0500

    update README with more instructions and resources

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import RegistrationForm from './components/RegistrationForm';
+import SignIn from './components/SignIn';
+import ContactUs from './components/ContactUs';
+import Feedback from './components/Feedback';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/registration" exact component ={RegistrationForm} />
+        <Route path="/registration" exact component={RegistrationForm} />
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/contact" exact component={ContactUs} />
+        <Route path="/feedback" exact component={Feedback} />
       </Router>
     </div>
   );
