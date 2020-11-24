aa2a9ab4d2e5eb1b8075773d13c5823ec7fce9b0
Jocelyn Jeriah <jocelynjeriah@jocelyns-mbp.myfiosgateway.com>
2020-11-24 15:27:50 -0500

    add 'react-bootstrap' and 'bootstrap' libraries, refactor registration form to implement new libraries, attempting to remove cached node_modules

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from './components/SignIn';
 import ContactUs from './components/ContactUs';
 import Feedback from './components/Feedback';
 import './App.css';
+import RegistrationForm2 from './components/RegistrationForm2';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signin" exact component={SignIn} />
         <Route path="/contact" exact component={ContactUs} />
         <Route path="/feedback" exact component={Feedback} />
+        <Route path="/test-form" exact component={RegistrationForm2} />
       </Router>
     </div>
   );
