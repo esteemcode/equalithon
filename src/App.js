5264ce9c9cfa5e6390bb074c5d81aad94cf2dc15
Laura-deVere <lauradevh@gmail.com>
2020-12-01 10:44:54 -0500

    Adding basic functionality for Sponsorhip form, D&I registration and Sponsors home page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import RegistrationForm from './components/RegistrationForm';
 import SignIn from './components/SignIn';
 import ContactUs from './components/ContactUs';
 import Feedback from './components/Feedback';
+import Sponsorship from './components/Sponsorship';
+import SponsorHome from './components/SponsorHome';
+import DAndIRegistration from './components/DAndIRegistration';
 import './App.css';
-import RegistrationForm2 from './components/RegistrationForm2';
+// import RegistrationForm2 from './components/RegistrationForm2';
 
 function App() {
   return (
@@ -14,7 +17,10 @@ function App() {
         <Route path="/signin" exact component={SignIn} />
         <Route path="/contact" exact component={ContactUs} />
         <Route path="/feedback" exact component={Feedback} />
-        <Route path="/test-form" exact component={RegistrationForm2} />
+        <Route path="/sponsorship" exact component={Sponsorship} />
+        <Route path="/sponsorhome" exact component={SponsorHome} />
+        <Route path="/dandiregistration" exact component={DAndIRegistration} />
+        {/* <Route path="/test-form" exact component={RegistrationForm2} /> */}
       </Router>
     </div>
   );
