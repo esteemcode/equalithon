d71adf751efcb2ca5657262235943e1cc4f623f2
Hafsat <hafsatajia@gmail.com>
2020-12-01 04:08:31 +0100

    Completed the skeletal html version of the Sign up form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SignIn from './components/SignIn';
 import ContactUs from './components/ContactUs';
 import Feedback from './components/Feedback';
 import './App.css';
-import RegistrationForm2 from './components/RegistrationForm2';
+
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/signin" exact component={SignIn} />
         <Route path="/contact" exact component={ContactUs} />
         <Route path="/feedback" exact component={Feedback} />
-        <Route path="/test-form" exact component={RegistrationForm2} />
+        
       </Router>
     </div>
   );
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,7 +4,37 @@ const SignIn = () => {
 
     return (
         <div>
-        </div>
+            <h3>SIGN UP</h3>
+        <form>       
+            <table>
+                <tr>
+                    <td>First name</td>
+                    <td><input type="text" placeholder= "First name" name=""/></td>
+                </tr>
+                <tr>
+                    <td>Last name</td>
+                    <td><input type="text" placeholder= "Last name" name=""/></td>
+                </tr>
+                <tr>
+                    <td>Email address</td>
+                    <td><input type="mail" placeholder= "Email" name=""/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" placeholder= "Password" name=""/></td>
+                </tr>
+                <tr>
+                    <td>Confirm password</td>
+                    <td><input type="password" placeholder= "Confirm password" name=""/></td>
+                </tr><br/>
+            </table>
+        
+                <p>Already signed up? <input type="submit" value="Login"/></p><br/>
+
+                <a href="#" class="google btn"><i class="fa fa-google fa-fw"></i> Login with Google+ </a><br/>
+                <a href="">Forgot Password?</a>
+         </form>
+         </div>
         
     )
 }
