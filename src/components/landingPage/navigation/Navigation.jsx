65986bb761ce4bf4a06d3923c41e225b20698148
JocelynJeriah <jocelyn.jeriah@gmail.com>
2020-12-15 17:16:28 -0500

    removed Home link from navigation

--- src/components/landingPage/navigation/Navigation.jsx
+++ src/components/landingPage/navigation/Navigation.jsx
@@ -19,7 +19,6 @@ const Navigation = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav>
-                <Nav.Link href="/" className="textdark">Home</Nav.Link>
                 <Nav.Link href="#link" className="text-dark">Link 1</Nav.Link>
                 <Nav.Link href="#link" className="text-dark">Link 2</Nav.Link>
                 </Nav>
