c201ec14ba98ea4d4f0a4f62137f19c3508afe98
Laura-deVere <lauradevh@gmail.com>
2020-12-16 09:08:13 -0500

    Adding the screen 3 after sign up and styling on sponsors home page

---
+++ src/components/RegistrationEndNavigation.js
@@ -0,0 +1,45 @@
+import { Container, Col, ListGroup, Row } from "react-bootstrap";
+import { Link } from 'react-router-dom'
+
+const RegistrationEndNavigation = () => {
+    // const handleOnSelect = () => {
+
+    // }
+    return (
+        <Container className="w-40">
+            <Row className="justify-content-md-center">
+                <Col lg={6} md={6} sm={6} xs={12}>
+                    <Row className="cyan form-header text-white pt-1 pb-1 col-12 mt-0 ml-0 mr-0 mb-3">
+                        <Row className="m-0 col-12">
+                            <h4>Thanks!</h4>
+                        </Row>
+                        <Row className="m-0 col-12">
+                            <h6>Please select among the following options</h6>
+                        </Row>
+                    </Row>
+                    <Row className="w-100 m-0">
+                        <ListGroup as="ul" className="w-100 m-0">
+                            <ListGroup.Item as="li" className="col-12 m-0 nav-list-item-w-hover-yellow mb-1 mt-1 border-top">
+                                <Link className="a-inherit">
+                                    I want to participate in an Equalithon
+                            </Link>
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" className="col-12 m-0 nav-list-item-w-hover-yellow mb-1 mt-1 border-top">
+                                <Link className="a-inherit">
+                                    I want to mentor an Equalithon
+                            </Link>
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" className="col-12 m-0 nav-list-item-w-hover-yellow mb-1 mt-1 border-top">
+                                <Link className="a-inherit">
+                                    I am a community manager
+                            </Link>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default RegistrationEndNavigation;
