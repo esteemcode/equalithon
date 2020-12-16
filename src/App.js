f94c4f7a34519ea7b4a0f0dfda25decb1ae43dee
JocelynJeriah <jocelyn.jeriah@gmail.com>
2020-12-16 10:39:19 -0500

    resolving App.js conflict

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import DAndIRegistration from './components/DAndIRegistration';
 import SponsorHome from './components/SponsorHome';
 import Sponsorship from './components/Sponsorship';
 import RegistrationEndNavigation from './components/RegistrationEndNavigation';
+import EqualithonSelection from './components/EqualithonSelection';
+import SelectLocation from './components/SelectLocation';
+import JobInfo from './components/JobInfo';
+import EqualithonInfo from './components/EqualithonInfo';
 
 function App() {
   return (
@@ -20,6 +24,10 @@ function App() {
         <Route path="/registrationendnavigation" exact component={RegistrationEndNavigation} />
         <Route path="/home" exact component={Home} />
         <Route path="/signin" exact component={SignIn} />
+        <Route path="/equalithon-selection" exact component={EqualithonSelection} />
+        <Route path="/select-location" exact component={SelectLocation} />
+        <Route path="/job-info" exact component={JobInfo} />
+        <Route path="/equalithon-info" exact component={EqualithonInfo} />
         <Route path="/contact" exact component={ContactUs} />
         <Route path="/feedback" exact component={Feedback} />
         <Route path="/group-registration" exact component={RegistrationGroup} />
